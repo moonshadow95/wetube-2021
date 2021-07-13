382eea0aacc68246f61c50ab057a59fb44d408f4
moonshadow <last_enemy@naver.com>
2021-07-13 13:37:36 +0900

    recap

--- src/index.js
+++ src/index.js
@@ -4,19 +4,23 @@ const PORT = 4000;
 
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} : ${req.url}`);
+const routeLogger = (req, res, next) => {
+  console.log(`ROUTE: ${req.path}`);
   next();
-}
+};
+
+const methodLogger = (req, res, next) => {
+  console.log(`METHOD: ${req.method}`);
+  next();
+};
 
 const handleHome = (req, res) => {
-  return res.send('I still love you.') 
-}
+  return res.send('I still love you.');
+};
 
-app.get('/', logger, handleHome)
+app.get('/', methodLogger, routeLogger, handleHome);
 
-const handleListening = () => 
-  console.log(`✅ Server Listening on port https://localhost:${PORT} 🚀`)
+const handleListening = () =>
+  console.log(`✅ Server Listening on port https://localhost:${PORT} 🚀`);
 
-
-app.listen(PORT, handleListening)
+app.listen(PORT, handleListening);
