481ebdeb294b84290c76210c5910624332e1c5e4
moonshadow <last_enemy@naver.com>
2021-07-14 17:27:29 +0900

    relative url and absoulte url

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,49 +1,54 @@
+let videos = [
+  {
+    title: 'Video #1',
+    rating: 5,
+    comments: 2,
+    creatAt: '2 minutes ago',
+    views: 512,
+    id: 1,
+  },
+  {
+    title: 'Video #2',
+    rating: 5,
+    comments: 2,
+    creatAt: '2 minutes ago',
+    views: 1,
+    id: 2,
+  },
+  {
+    title: 'Video #3',
+    rating: 5,
+    comments: 2,
+    creatAt: '2 minutes ago',
+    views: 1,
+    id: 3,
+  },
+  {
+    title: 'Video #4',
+    rating: 5,
+    comments: 2,
+    creatAt: '2 minutes ago',
+    views: 512,
+    id: 4,
+  },
+  {
+    title: 'Video #5',
+    rating: 5,
+    comments: 2,
+    creatAt: '2 minutes ago',
+    views: 512,
+    id: 5,
+  },
+];
+
 export const recommended = (req, res) => {
-  const videos = [
-    {
-      title: 'Video #1',
-      rating: 5,
-      comments: 2,
-      creatAt: '2 minutes ago',
-      views: 512,
-      id: 1,
-    },
-    {
-      title: 'Video #2',
-      rating: 5,
-      comments: 2,
-      creatAt: '2 minutes ago',
-      views: 512,
-      id: 1,
-    },
-    {
-      title: 'Video #3',
-      rating: 5,
-      comments: 2,
-      creatAt: '2 minutes ago',
-      views: 512,
-      id: 1,
-    },
-    {
-      title: 'Video #4',
-      rating: 5,
-      comments: 2,
-      creatAt: '2 minutes ago',
-      views: 512,
-      id: 1,
-    },
-    {
-      title: 'Video #5',
-      rating: 5,
-      comments: 2,
-      creatAt: '2 minutes ago',
-      views: 512,
-      id: 1,
-    },
-  ];
   return res.render('home', { pageTitle: 'Home', videos });
 };
-export const see = (req, res) => res.render('watch', { pageTitle: 'Watch' });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render('watch', { pageTitle: `Watching ${video.title}`, video });
+};
 export const edit = (req, res) => res.render('edit', { pageTitle: 'Edit' });
 export const search = (req, res) => res.send('Search');
 export const upload = (req, res) => res.send('Upload');
--- src/router/videoRouter.js
+++ src/router/videoRouter.js
@@ -1,11 +1,16 @@
 import express from 'express';
-import { deleteVideo, edit, upload, see } from '../controllers/videoController';
+import {
+  deleteVideo,
+  edit,
+  upload,
+  watch,
+} from '../controllers/videoController';
 
 const videoRouter = express.Router();
 
-videoRouter.get('/upload', upload);
-videoRouter.get('/:id(\\d+)', see);
+videoRouter.get('/:id(\\d+)', watch);
 videoRouter.get('/:id(\\d+)/edit', edit);
 videoRouter.get('/:id(\\d+)/delete', deleteVideo);
+videoRouter.get('/upload', upload);
 
 export default videoRouter;
