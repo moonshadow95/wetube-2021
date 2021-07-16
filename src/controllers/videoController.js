17ecce81e182d6933ff1ef56fe17871994298445
moonshadow <last_enemy@naver.com>
2021-07-16 12:33:02 +0900

    db set (mongoDB)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,62 +1,79 @@
 let videos = [
   {
-    title: 'Video #1',
+    title: "Video #1",
     rating: 5,
     comments: 2,
-    creatAt: '2 minutes ago',
+    creatAt: "2 minutes ago",
     views: 512,
+    id: 0,
+  },
+  {
+    title: "Video #2",
+    rating: 5,
+    comments: 2,
+    creatAt: "2 minutes ago",
+    views: 1,
     id: 1,
   },
   {
-    title: 'Video #2',
+    title: "Video #3",
     rating: 5,
     comments: 2,
-    creatAt: '2 minutes ago',
+    creatAt: "2 minutes ago",
     views: 1,
     id: 2,
   },
   {
-    title: 'Video #3',
+    title: "Video #4",
     rating: 5,
     comments: 2,
-    creatAt: '2 minutes ago',
-    views: 1,
+    creatAt: "2 minutes ago",
+    views: 512,
     id: 3,
   },
   {
-    title: 'Video #4',
+    title: "Video #5",
     rating: 5,
     comments: 2,
-    creatAt: '2 minutes ago',
+    creatAt: "2 minutes ago",
     views: 512,
     id: 4,
-  },
-  {
-    title: 'Video #5',
-    rating: 5,
-    comments: 2,
-    creatAt: '2 minutes ago',
-    views: 512,
-    id: 5,
   },
 ];
 
 export const recommended = (req, res) => {
-  return res.render('home', { pageTitle: 'Home', videos });
+  return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render('watch', { pageTitle: `Watching ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render('edit', { pageTitle: `Editing ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
+  videos[id].title = title;
   return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  // here i will add a video to the videos array
+  const newVideo = {
+    title: req.body.title,
+    rating: 5,
+    comments: 2,
+    creatAt: "2 minutes ago",
+    views: 512,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
 };
--- src/server.js
+++ src/server.js
@@ -1,20 +1,21 @@
-import express from 'express';
-import morgan from 'morgan';
-import globalRouter from './router/globalRouter';
-import userRouter from './router/userRouter';
-import videoRouter from './router/videoRouter';
+import "./db";
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./router/globalRouter";
+import userRouter from "./router/userRouter";
+import videoRouter from "./router/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
-const logger = morgan('dev');
-app.set('views', process.cwd() + '/src/views');
-app.set('view engine', 'pug');
+const logger = morgan("dev");
+app.set("views", process.cwd() + "/src/views");
+app.set("view engine", "pug");
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
-app.use('/', globalRouter);
-app.use('/videos', videoRouter);
-app.use('/users', userRouter);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`✅ Server Listening on port https://localhost:${PORT} 🚀`);
