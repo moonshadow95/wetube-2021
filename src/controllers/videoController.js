519e3d94fee57bf5e5984fdaf6cec3b57f0e1604
moonshadow <last_enemy@naver.com>
2021-07-18 12:48:47 +0900

    video model and uploading

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,79 +1,37 @@
-let videos = [
-  {
-    title: "Video #1",
-    rating: 5,
-    comments: 2,
-    creatAt: "2 minutes ago",
-    views: 512,
-    id: 0,
-  },
-  {
-    title: "Video #2",
-    rating: 5,
-    comments: 2,
-    creatAt: "2 minutes ago",
-    views: 1,
-    id: 1,
-  },
-  {
-    title: "Video #3",
-    rating: 5,
-    comments: 2,
-    creatAt: "2 minutes ago",
-    views: 1,
-    id: 2,
-  },
-  {
-    title: "Video #4",
-    rating: 5,
-    comments: 2,
-    creatAt: "2 minutes ago",
-    views: 512,
-    id: 3,
-  },
-  {
-    title: "Video #5",
-    rating: 5,
-    comments: 2,
-    creatAt: "2 minutes ago",
-    views: 512,
-    id: 4,
-  },
-];
+import Video from "../models/Video";
 
-export const recommended = (req, res) => {
+export const home = async (req, res) => {
+  const videos = await Video.find({});
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching` });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id].title = title;
   return res.redirect(`/videos/${id}`);
 };
-
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 export const postUpload = (req, res) => {
-  // here i will add a video to the videos array
-  const newVideo = {
-    title: req.body.title,
-    rating: 5,
-    comments: 2,
-    creatAt: "2 minutes ago",
-    views: 512,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
+  const { title, description, hashtags } = req.body;
+  const video = new Video({
+    title,
+    description,
+    hashtags: hashtags.split(",").map((tag) => `#${tag}`),
+    createAt: Date.now(),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  console.log(video);
   return res.redirect("/");
 };
--- src/router/globalRouter.js
+++ src/router/globalRouter.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import { join, login } from '../controllers/userController';
-import { recommended } from '../controllers/videoController';
+import express from "express";
+import { join, login } from "../controllers/userController";
+import { home } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
-globalRouter.get('/', recommended);
-globalRouter.get('/join', join);
-globalRouter.get('/login', login);
+globalRouter.get("/", home);
+globalRouter.get("/join", join);
+globalRouter.get("/login", login);
 
 export default globalRouter;
