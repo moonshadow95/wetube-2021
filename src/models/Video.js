1ba56307454a8926dfb66a681d5865d57268244c
moonshadow <last_enemy@naver.com>
2021-07-19 17:20:22 +0900

    mongoose middleware for save hashtags

--- src/models/Video.js
+++ src/models/Video.js
@@ -11,5 +11,12 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
+videoSchema.pre("save", async function () {
+  this.hashtags = this.hashtags[0]
+    .split(",")
+    .map((tag) => (tag.startsWith("#") ? tag : `#${tag}`));
+});
+
 const movieModel = mongoose.model("Video", videoSchema);
+
 export default movieModel;
