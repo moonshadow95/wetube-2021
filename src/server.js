a67a374363041cf531b5efb9bd4ee9b8fe5c2437
moonshadow <last_enemy@naver.com>
2021-07-22 18:46:45 +0900

    sessions

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./router/rootRouter";
 import userRouter from "./router/userRouter";
 import videoRouter from "./router/videoRouter";
@@ -10,6 +11,21 @@ app.set("views", process.cwd() + "/src/views");
 app.set("view engine", "pug");
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
+app.use(session({ secret: "Hello!", resave: true, saveUninitialized: true }));
+app.use((req, res, next) => {
+  console.log(req.headers);
+  next();
+});
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+app.get("/add-one", (req, res, next) => {
+  req.session.potato += 1;
+  return res.send(`${req.session.id}`);
+});
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
