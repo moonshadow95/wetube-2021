4580b1803dbb0bd171097cbb474d3b8013047ff4
moonshadow <last_enemy@naver.com>
2021-07-23 16:38:40 +0900

    make session only for logged in user

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./router/rootRouter";
 import userRouter from "./router/userRouter";
 import videoRouter from "./router/videoRouter";
@@ -12,7 +13,14 @@ app.set("views", process.cwd() + "/src/views");
 app.set("view engine", "pug");
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
-app.use(session({ secret: "Hello!", resave: true, saveUninitialized: true }));
+app.use(
+  session({
+    secret: "Hello!",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
+  })
+);
 app.use((req, res, next) => {
   console.log(req.headers);
   next();
