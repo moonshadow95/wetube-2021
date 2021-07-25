6dcc80099fa4fccd9aa56540d5868fb2d5700ee1
moonshadow <last_enemy@naver.com>
2021-07-25 18:01:49 +0900

    get a code - change to access_token -use github api - get user info

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   edit,
+  finishGithubLogin,
   logout,
   remove,
   see,
@@ -13,6 +14,7 @@ userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
 userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id", see);
 
 export default userRouter;
