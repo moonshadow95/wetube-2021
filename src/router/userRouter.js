17a5da8430342b40356da866a2be753b46423417
moonshadow <last_enemy@naver.com>
2021-07-26 18:03:18 +0900

    edit profile page

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -5,12 +5,14 @@ import {
   see,
   finishGithubLogin,
   startGithubLogin,
+  getEdit,
+  postEdit,
 } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
+userRouter.route("/edit").get(getEdit).post(postEdit);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id", see);
