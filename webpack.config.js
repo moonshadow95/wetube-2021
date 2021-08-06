ad891aaa303a61ec828551087d710ed573007051
moonshadow <last_enemy@naver.com>
2021-08-06 16:03:44 +0900

    webpack (babel, scss, miniCss)

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  // source code (src)
+  entry: "./src/client/js/main.js",
+  mode: "development",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
+  // filename: 변경할 파일, path: 저장할 위치
+  output: {
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
