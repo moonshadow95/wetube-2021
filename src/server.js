44f5f1e84c3065dc8be5566497f2445b36f1ebac
moonshadow <last_enemy@naver.com>
2021-08-23 17:50:20 +0900

    CROS Issue

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
   );
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
-
+  res.header("Access-Control-Allow-Origin", "no-cors");
   next();
 });
 app.use("/uploads", express.static("uploads"));
