cd4d28587272e88957d5cb27187a8765dbf95230
moonshadow <last_enemy@naver.com>
2021-08-24 13:11:21 +0900

    CROS Issue

--- src/server.js
+++ src/server.js
@@ -33,10 +33,7 @@ app.use((req, res, next) => {
   );
   res.header("Cross-Origin-Embedder-Policy", "credentialless");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://wetube-moonshadow.s3.amazonaws.com"
-  );
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 app.use("/uploads", express.static("uploads"));
