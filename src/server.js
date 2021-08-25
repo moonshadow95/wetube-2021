e307d8c15ed8e9d934e18f176fa8f94723d7ce44
moonshadow <last_enemy@naver.com>
2021-08-25 15:11:26 +0900

    test

--- src/server.js
+++ src/server.js
@@ -28,6 +28,7 @@ app.use(flash());
 app.use(localsMiddleware);
 app.use((req, res, next) => {
   res.header("Cross-Origin-Resource-Policy", "*");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
   next();
 });
 app.use("/uploads", express.static("uploads"));
