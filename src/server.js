dabf2945882187ba5700190a41403561e24d2074
moonshadow <last_enemy@naver.com>
2021-08-26 11:22:54 +0900

    coop

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ app.use("/static", express.static("assets"));
 app.use("/ffmpeg", express.static("node_modules/@ffmpeg/core/dist"));
 app.use((req, res, next) => {
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
-  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  res.header("Cross-Origin-Opener-Policy", "cross-origin");
   next();
 });
 
